30d76ffce80ed74a0dd944277f9f17f80112720b
Ever Fernando Garay Molinas <everfgmolinas@hotmail.com>
2022-05-29 15:56:26 -0400

    feat: define relations

--- app/models/index.js
+++ app/models/index.js
@@ -19,7 +19,68 @@ db.Clientes = require("./cliente.model.js")(sequelize, Sequelize);
 //db.Categorias = require("./categoria.model.js")(sequelize,Sequelize);
 //db.Productos = require("./producto.model.js")(sequelize,Sequelize);
 db.Mesas = require("./mesa.models.js")(sequelize, Sequelize);
-//db.Reservas = require("./reserva.model.js")(sequelize, Sequelize);
+db.Reservas = require("./reserva.model.js")(sequelize, Sequelize);
+db.Horas_Reservas = require("./horas_reserva.model.js")(sequelize, Sequelize);
+
+db.Restaurantes.hasMany(db.Mesas, {
+  foreignKey: 'restaurante_id',
+  sourceKey: 'id',
+});
+
+db.Restaurantes.hasMany(db.Reservas, {
+  foreignKey: 'restaurante_id',
+  sourceKey: 'id',
+});
+
+db.Reservas.hasMany(db.Horas_Reservas, {
+  foreignKey: 'reserva_id',
+  sourceKey: 'id',
+});
+
+db.Mesas.hasMany(db.Reservas, {
+  foreignKey: 'mesa_id',
+  sourceKey: 'id',
+});
+
+db.Clientes.hasMany(db.Reservas, {
+  foreignKey: 'cliente_id',
+  sourceKey: 'id',
+});
+
+
+
+
+
+db.Mesas.belongsTo(db.Restaurantes, {
+  foreignKey: "restaurante_id",
+  targetKey: 'id',
+  onDelete: 'CASCADE',
+});
+
+db.Reservas.belongsTo(db.Restaurantes, {
+  foreignKey: "restaurante_id",
+  targetKey: 'id',
+  onDelete: 'CASCADE',
+});
+
+db.Horas_Reservas.belongsTo(db.Reservas,{
+  foreignKey: 'reserva_id',
+  targetKey: 'id',
+  onDelete: 'CASCADE',
+});
+
+db.Reservas.belongsTo(db.Mesas, {
+  foreignKey: "mesa_id",
+  targetKey: 'id',
+  onDelete: 'CASCADE',
+});
+
+db.Reservas.belongsTo(db.Clientes, {
+  foreignKey: "cliente_id",
+  targetKey: 'id',
+  onDelete: 'CASCADE',
+});
+
 //db.Consumos = require("./consumo.model.js")(sequelize, Sequelize);
 //db.Detalles = require("./detalle_consumo.model.js")(sequelize, Sequelize);
 module.exports = db;
