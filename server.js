f92a64742386cbe00d487e54ab58349ad3b776c8
Ever Fernando Garay Molinas <everfgmolinas@hotmail.com>
2022-05-29 15:57:35 -0400

    feat: add reserva routes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 require("./app/routes/mesa.routes")(app);
 require("./app/routes/restaurante.routes")(app);
 require("./app/routes/cliente.routes")(app);
-//require("./app/routes/reserva.routes")(app);
+require("./app/routes/reserva.routes")(app);
 //require("./app/routes/consumo.routes")(app);
 //require("./app/routes/detalle.routes")(app);
 //require("./app/routes/producto.routes")(app);
