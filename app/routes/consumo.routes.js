902a30c837b1f21ba6dc4658dc24e1e32ba9f867
carinme <carinme97@fpuna.edu.py>
2022-06-21 16:42:30 -0400

    Rutas correspondientes al modulo de consumo

--- app/routes/consumo.routes.js
+++ app/routes/consumo.routes.js
@@ -1,13 +1,11 @@
-module.exports = app =>{
-    const consumoDao = require('../controllers/consumo.controller');
-    const router = require('express').Router();
-    router.get("/", consumoDao.findAll);
-    router.get("/:id", consumoDao.findOne);
-    router.get("/detalle/:id_cosumo", consumoDao.getDetalles);
-    router.get("/estaOcupada/:id_mesa", consumoDao.isTakenRest);
-    router.post('/', consumoDao.create);
-    router.post('/addDetalle', consumoDao.addDetalles);
-    router.put('/', consumoDao.update);
-    router.put('/cerrarMesa/:id_consumo', consumoDao.cerrarMesa);
+module.exports = app => {
+    const consumo = require("../controllers/consumodao.controller.js");
+    var router = require("express").Router();
+    router.post("/", consumo.create);
+    router.put("/:id", consumo.update);
+    router.get("/:id/factura",consumo.factura);
+    router.get("/:id", consumo.findIDCliente);
+    router.get("/ocupado/:id", consumo.findOcupado);
+    router.get("/", consumo.findAll);
     app.use('/api/consumo', router);
-}
+};
